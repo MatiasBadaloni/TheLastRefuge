b0ab9637c9f7259399303f640189363361f9bcdf
Matias Badaloni <53199112+MatiasBadaloni@users.noreply.github.com>
2021-02-18 09:35:02 -0300

    Sistema de literatura
    ahora puedes leer parrafos

---
+++ Assets/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Libro : MonoBehaviour
+{
+    public Text texto;
+    public Parrafo parrafo;
+
+    void Start()
+    {
+        Actualizar();
+    }
+
+    public void Actualizar()
+    {
+        texto.text = parrafo.parrafo.ToString();
+    }
+    
+      
+    
+}
